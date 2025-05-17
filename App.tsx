05b40c06763e1a98bcf2c52613aab7b4ea615e52
RemoStain <c.wise522@mybvc.ca>
2025-05-17 13:14:25 -0600

    Add bvc-MAD-p1 directory (non-git)

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import HomeScreen from './src/screens/HomeScreen';
+import Navbar from './src/components/Navbar';
 
 const App = () => {
-  return <HomeScreen />;
+  return (
+    <>
+      <Navbar />
+      <HomeScreen />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#282c34', color: 'white' }}>
+      <h1>My App</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
